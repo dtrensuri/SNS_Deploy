d91b6180a3a6b895e969510c050821b463d1e0c1
dtrensuri <dothanhcao2502@gmail.com>
2023-11-03 08:02:12 +0700

    new

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // $this->call(UserSeed::class);
-        $this->call(AccountSeed::class);
+        $this->call(UserSeed::class);
+        // $this->call(AccountSeed::class);
     }
 }
