e39e72f461d7f38a3383c4983ab1b67f6e3d1ded
dtrensuri <dothanhcao2502@gmail.com>
2023-11-09 16:49:42 +0700

    new

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -61,7 +61,6 @@ class FacebookController extends Controller
 
         $helper = $this->client->getRedirectLoginHelper();
         $pdata = $helper->getPersistentDataHandler();
-        dd($request, $request->get('state'));
         $pdata->set('state', $request->get('state'));
         try {
             $accessToken = $helper->getAccessToken();
@@ -73,7 +72,7 @@ class FacebookController extends Controller
             exit;
         }
         if (isset($accessToken)) {
-            return response()->json($request);
+            return response()->json($accessToken);
         }
         // return response()->json($request);
     }
