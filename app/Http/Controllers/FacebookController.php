d4cee318ca604f024eaf203d56eae338369d1b7e
dtrensuri <dothanhcao2502@gmail.com>
2023-11-09 15:49:02 +0700

    new

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -54,21 +54,20 @@ class FacebookController extends Controller
 
     public function loginCallback(Request $request)
     {
-        Log::info('Facebook Login Callback');
-        $helper = $this->client->getRedirectLoginHelper();
-
-        try {
-            $accessToken = $helper->getAccessToken();
-        } catch (FacebookResponseException $e) {
-            echo 'Graph returned an error: ' . $e->getMessage();
-            exit;
-        } catch (FacebookSDKException $e) {
-            echo 'Facebook SDK returned an error: ' . $e->getMessage();
-            exit;
-        }
-        if (isset($accessToken)) {
-            dd($accessToken);
-        }
+        // Log::info('Facebook Login Callback');
+        // $helper = $this->client->getRedirectLoginHelper();
+        // try {
+        //     $accessToken = $helper->getAccessToken();
+        // } catch (FacebookResponseException $e) {
+        //     echo 'Graph returned an error: ' . $e->getMessage();
+        //     exit;
+        // } catch (FacebookSDKException $e) {
+        //     echo 'Facebook SDK returned an error: ' . $e->getMessage();
+        //     exit;
+        // }
+        // if (isset($accessToken)) {
+        //     dd($accessToken);
+        // }
         return response()->json($request);
     }
 
