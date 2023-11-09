ee74d046b69a057d7cfa44a85287c4f85ec2b2b8
dtrensuri <dothanhcao2502@gmail.com>
2023-11-09 12:38:27 +0700

    fix

--- resources/views/user/setting/channel.blade.php
+++ resources/views/user/setting/channel.blade.php
@@ -154,9 +154,6 @@
                         } else {
                             console.log('Login failed');
                         }
-                    }, {
-                        scope: 'pages_manage_ads,pages_manage_metadata,pages_read_engagement,pages_read_user_content,pages_manage_posts,pages_manage_engagement,pages_messaging,pages_show_list,read_insights,email,instagram_basic,instagram_content_publish,instagram_manage_messages,instagram_manage_comments,instagram_manage_insights,instagram_shopping_tag_products,publish_to_groups',
-                        return_scopes: true,
                     });
                 } catch (e) {
                     console.log(e);
